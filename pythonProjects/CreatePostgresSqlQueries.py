f009f820198100ea64ba6a2170f1265f6fcca772
VivekGupta335 <vivek.gupta335@gmail.com>
2023-06-15 01:03:54 -0400

    Update CreatePostgresSqlQueries.py

--- pythonProjects/CreatePostgresSqlQueries.py
+++ pythonProjects/CreatePostgresSqlQueries.py
@@ -6,6 +6,8 @@ _ = load_dotenv(find_dotenv())
 
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 
+text = input("prompt")
+
 def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
@@ -23,7 +25,8 @@ prompt = f"""
 # Department(id, name, address)
 # Salary_Payments(id, employee_id, amount, date)
 #
-### A query to create all 3 tables:
+Create Query based on query, which is delimited with triple brackets
+query: '''{text}'''
 """
 
 response = get_completion(prompt)
@@ -31,6 +34,8 @@ print(response)
 
 
 #sample response
+
+#text=A query to create all tables
 # CREATE TABLE Employee (
 #   id SERIAL PRIMARY KEY,
 #   name VARCHAR(50) NOT NULL,
@@ -49,4 +54,15 @@ print(response)
 #   amount DECIMAL(10,2) NOT NULL,
 #   date DATE NOT NULL
 # );
+
+#text=A query to list the names of the departments which employed more than 10 employees in the last 3 months
+# SELECT Department.name 
+# FROM Department 
+# INNER JOIN Employee ON Department.id = Employee.department_id 
+# INNER JOIN Salary_Payments ON Employee.id = Salary_Payments.employee_id 
+# WHERE Salary_Payments.date >= NOW() - INTERVAL '3 months' 
+# GROUP BY Department.name 
+# HAVING COUNT(Employee.id) > 10;
+
+
 
